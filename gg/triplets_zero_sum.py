9be11d40643d226aa7661cad48dbdfa54dc6cb60
haileyneorsd <128643117+haileyneorsd@users.noreply.github.com>
2023-05-22 08:56:22 -0400

    solution

--- gg/triplets_zero_sum.py
+++ gg/triplets_zero_sum.py
@@ -1,17 +1,29 @@
 
 #https://practice.geeksforgeeks.org/problems/find-triplets-with-zero-sum/1?page=1&difficulty[]=-1&sortBy=submissions
 
+#using permutations is not efficient bc returns all combinations of arr, and adding digits is the same result regardless of the order of the digits.
+#arr.sort() to sort array in ascending order
+#if sum>0, the sum is too high so decrease sum by decreasing highest digit (second-=1)
+#else (if sum<0) the sum is too low so increase sum by increasing lowest digit (first+=1). 
+
 from itertools import permutations
 
-#Function to find triplets with zero sum.    
-def findTriplets(arr, n):
-    #code here
-    perms = list(permutations(arr,3))
-    for i in perms:
-        if sum(i) == 0:
-            return True
+#Function to find triplets with zero sum.   
+def findTriplets(arr, n): 
+    arr.sort()
+    for i in range(n):
+        first=i+1
+        second=n-1 
+        while first<second:
+            sum=arr[i]+arr[first]+arr[second]
+            if sum==0:
+                return True
+            elif sum>0:
+                second-=1 
+            else:
+                first+=1 
+            
     return False
-    #print(perms)
 
 if __name__ == '__main__':
 
